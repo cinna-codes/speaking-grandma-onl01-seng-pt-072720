be4c1b48f529d593bbd6431cef51f48e91e442c6
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-07-29 18:37:32 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,10 +15,11 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  elseif phrase == phrase.upcase
+    elseif phrase == phrase.upcase
   puts "NO, NOT SINCE 1938!"
 else
   puts "HUH?! SPEAK UP, SONNY!"
+end
 end
 
 speak_to_grandma("i love you grandma!".upcase)
